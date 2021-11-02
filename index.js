bc8de8052341096385696b9a12f247f1213b34db
Gustavo Novaes <gustavonovaes.dev@gmail.com>
2021-11-02 08:22:40 -0300

    add graceful exit to nodejs

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ const app = require('./src/app');
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening...');
 });
+
+const exitHandler = () => {
+  if (app) {
+    return app.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+};
+
+const unexpectedErrorHandler = () => {
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  if (app) {
+    app.close();
+  }
+});
